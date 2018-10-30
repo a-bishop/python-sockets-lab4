575c6ced2abd6c19e7d2c8e23cff604c100abc11
Andrew Bishop <andrew.bishop53@gmail.com>
2018-10-30 14:21:02 -0700

    added clientHandler and Manager classes

--- server.py
+++ server.py
@@ -5,27 +5,152 @@ import sys
 import socket
 import os
 import collections
+from threading import Thread
 
 # declare variables
 port = int(sys.argv[1])
-if len(sys.argv) > 2:
-    verbose = True if sys.argv[2] == '-v' else False
+numClients = int(sys.argv[2])
+if "-v" in sys.argv:
+    verbose = True
 else:
     verbose = False
 
+# open socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bind socket to host and port
+s.bind(('', port))
+# listen
+s.listen(numClients) # <-- how many concurrent clients can be 
+            # queued, waiting for handshake
+
+if verbose:
+    print('server waiting on port %s' % port)
+    print('number of concurrent clients: %s' % numClients)
+
+# Your server, when it receives a new client connection with the sock.accept() method,
+# creates a client handler thread but does not start it! It simply hands the waiting client thread to
+# the “manager”.
+
+for num in range(1, numClients):
+    conn, addr = s.accept()
+    if verbose:
+        print('server connected to client at ' + addr[0] + ':' + str(addr[1]))
+    thread = ClientHandler(num, conn, addr)
+    manager.addThread(thread)
+
 # The ClientHandler class
 
-    # You must create a subclass of threading.Thread to handle all communication with a single
-    # client.
+# You must create a subclass of threading.Thread to handle all communication with a single
+# client.
 
-    # Your server, when it receives a new client connection with the sock.accept() method,
-    # creates a client handler thread but does not start it! It simply hands the waiting client thread to
-    # the “manager”.
+class ClientHandler(Thread):
+
+    def __init__(self):
+        Thread.__init__(self, num, conn, addr)
+        self.num = num
+        self.conn = conn
+        self.addr = addr
+
+    def run(self):
+        print "Starting " + self.name
+
+        # function definitions
+        def recvWriteFile(filename, conn, size):
+            with open(filename, "wb") as f:
+                dataRemaining = size
+                while dataRemaining > 0:
+                    data = conn.recv(min(1024, dataRemaining))
+                    if (dataRemaining == len(data)):
+                        f.write(data)
+                        conn.send("DONE".encode())
+                        dataRemaining = 0
+                    else:
+                        f.write(data)
+                        dataRemaining -= len(data)
+
+        def readSendFile(filename, conn, size):
+            with open(filename, "rb") as f:
+                dataRemaining = size
+                while dataRemaining > 0:
+                    data = f.read(min(1024, dataRemaining))
+                    dataRemaining -= len(data)
+                    if (dataRemaining == 0):
+                        conn.send(data)
+                        conn.send("DONE".encode())
+                        break
+                    else:
+                        conn.send(data)
+
+            # send READY message
+        conn.send('READY'.encode())
+
+        data = conn.recv(1024).decode()
+        data = data.split(' ')
+        request = data[0]
+        filename = data[1]
+        filepath = './'
+
+        ### ---- handle GET requests ----- ###
+        if request == 'GET':
+            if verbose:
+                print('server receiving request: ' + request)
+            # check if file requested is on server
+            if os.path.isfile(filename) == True:
+                conn.send('OK'.encode())
+                response = conn.recv(1024).decode()
+                if response == 'READY':
+                    size = os.path.getsize(filename)
+                    size_bytes = size.to_bytes(8, byteorder='big', signed=False)
+                    conn.send(size_bytes)
+                    response = conn.recv(1024).decode()
+                    if response == 'OK':
+                        if verbose:
+                            print('server sending %d bytes' % size)
+                        readSendFile(filename, conn, size)
+            else:
+                msg = 'ERROR: file %s does not exist' % filename
+                conn.send(msg.encode())
+                
+        ### --- Handle PUT Requests --- ###
+        elif request == 'PUT':
+            if verbose:
+                print('server receiving request: ' + request)
+            # check if OS can create file (permissions, etc)
+            if os.access(filepath, os.X_OK):
+                conn.send('OK'.encode())
+                # receive number of bytes
+                size = int.from_bytes(conn.recv(8), byteorder='big', signed=False)
+                if verbose:
+                    print('server receiving %d bytes' % size)
+                conn.send('OK'.encode())
+                # receive bytes in 1024 blocks
+                recvWriteFile(filename, conn, size)
+            else:
+                msg = 'ERROR: unable to create file %s' % filename
+                conn.send(msg.encode())
+            
+
+        ### --- Handle DEL Requests --- ###
+        elif request == 'DEL':
+            if verbose:
+                print('server receiving request: ' + request)
+            # check permissions
+            try:
+                print('server deleting file %s' % filename)
+                os.remove(filename)
+                conn.send('DONE'.encode())
+            except:
+                msg = 'ERROR: unable to delete %s' % filename
+                conn.send(msg.encode())
+    
+        print "Exiting " + self.name
 
 # The Manager class
 
     # This class will maintain two data structures: a queue (implemented with a Python
     # collections.deque() object), and a set (implemented with a Python set() object).
+
+class Manager
 
     self.q = collections.deque()
     self.running = set()
@@ -51,11 +176,11 @@ else:
 # The manager sits in an infinite loop, executing the following pseudocode:
 
 # check the “ running ” threads; if any of them have stopped, remove them from the set.
-kick = []
-for t in self.running:
-    if not t.isAlive(): kick.append(t)
-for t in kick:
-    self.running.remove(t)
+# kick = []
+# for t in self.running:
+#     if not t.isAlive(): kick.append(t)
+# for t in kick:
+#     self.running.remove(t)
 
 # check the waiting queue:
     # if empty, sleep for 1 second and return to the top of the loop;
@@ -75,108 +200,6 @@ for t in kick:
     # kill <pid> - forces all threads under that process id to stop.
 
 # declare functions
-def recvWriteFile(filename, conn, size):
-    with open(filename, "wb") as f:
-        dataRemaining = size
-        while dataRemaining > 0:
-            data = conn.recv(min(1024, dataRemaining))
-            if (dataRemaining == len(data)):
-                f.write(data)
-                conn.send("DONE".encode())
-                dataRemaining = 0
-            else:
-                f.write(data)
-                dataRemaining -= len(data)
 
-def readSendFile(filename, conn, size):
-    with open(filename, "rb") as f:
-        dataRemaining = size
-        while dataRemaining > 0:
-            data = f.read(min(1024, dataRemaining))
-            dataRemaining -= len(data)
-            if (dataRemaining == 0):
-                conn.send(data)
-                conn.send("DONE".encode())
-                break
-            else:
-                conn.send(data)
-
-# open socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind socket to host and port
-s.bind(('', port))
-# listen
-s.listen(0) # <-- how many concurrent clients can be 
-            # queued, waiting for handshake
-
-if verbose:
-    print('server waiting on port %s' % port)
-
-while True:
-
-    # accept and send READY message
-    conn, addr = s.accept() # creates a new socket, connected to client
-    if verbose:
-        print('server connected to client at ' + addr[0] + ':' + str(addr[1]))
-    conn.send('READY'.encode())
-
-    data = conn.recv(1024).decode()
-    data = data.split(' ')
-    request = data[0]
-    filename = data[1]
-    filepath = './'
-
-    ### ---- handle GET requests ----- ###
-    if request == 'GET':
-        if verbose:
-            print('server receiving request: ' + request)
-        # check if file requested is on server
-        if os.path.isfile(filename) == True:
-            conn.send('OK'.encode())
-            response = conn.recv(1024).decode()
-            if response == 'READY':
-                size = os.path.getsize(filename)
-                size_bytes = size.to_bytes(8, byteorder='big', signed=False)
-                conn.send(size_bytes)
-                response = conn.recv(1024).decode()
-                if response == 'OK':
-                    if verbose:
-                        print('server sending %d bytes' % size)
-                    readSendFile(filename, conn, size)
-        else:
-            msg = 'ERROR: file %s does not exist' % filename
-            conn.send(msg.encode())
-            
-    ### --- Handle PUT Requests --- ###
-    elif request == 'PUT':
-        if verbose:
-            print('server receiving request: ' + request)
-        # check if OS can create file (permissions, etc)
-        if os.access(filepath, os.X_OK):
-            conn.send('OK'.encode())
-            # receive number of bytes
-            size = int.from_bytes(conn.recv(8), byteorder='big', signed=False)
-            if verbose:
-                print('server receiving %d bytes' % size)
-            conn.send('OK'.encode())
-            # receive bytes in 1024 blocks
-            recvWriteFile(filename, conn, size)
-        else:
-            msg = 'ERROR: unable to create file %s' % filename
-            conn.send(msg.encode())
-        
-
-    ### --- Handle DEL Requests --- ###
-    elif request == 'DEL':
-        if verbose:
-            print('server receiving request: ' + request)
-        # check permissions
-        try:
-            print('server deleting file %s' % filename)
-            os.remove(filename)
-            conn.send('DONE'.encode())
-        except:
-            msg = 'ERROR: unable to delete %s' % filename
-            conn.send(msg.encode())
 
             
