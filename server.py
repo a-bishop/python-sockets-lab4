4b74039a750ccff5cf9bcaeb28ec94b2019c4166
Andrew Bishop <andrew.bishop53@gmail.com>
2018-11-02 16:13:00 -0700

    cleanup

--- server.py
+++ server.py
@@ -7,11 +7,8 @@ import socket
 import os
 import collections
 import time
-from threading import Thread, Lock
+from threading import Thread
 
-# Your server, when it receives a new client connection with the sock.accept() method,
-# creates a client handler thread but does not start it! It simply hands the waiting client thread to
-# the “manager”.
 class ClientHandler(Thread):
 
     def __init__(self, conn):
@@ -194,66 +191,6 @@ if __name__ == '__main__':
             print('server connected to client at ' + addr[0] + ':' + str(addr[1]))
         thread = ClientHandler(conn)
         clientManager.add(thread)
-    
-
-# The ClientHandler class
-
-# You must create a subclass of threading.Thread to handle all communication with a single
-# client.
-
-
-
-# The Manager class
-
-    # This class will maintain two data structures: a queue (implemented with a Python
-    # collections.deque() object), and a set (implemented with a Python set() object).
-
-
-
-    # The queue will hold all the waiting client connections; they haven’t started yet, they’ve just been
-    # added to the queue, waiting to be executed. The main program calls a method of this class to
-    # add new client threads to the queue.
-
-    # When the manager decides it is time to start one of the waiting client threads, it issues the
-    # t.start() command, and then adds it to the “ running ” set.
-
-    # (Sets are kind of like queues, except they don’t maintain any particular order; you add items,
-    # remove items, and iterate efficiently, but you can’t expect any particular order when you iterate.)
-
-    # We will impose a limit on how big the “ running ” set may be, so that the clients are limited to
-    # how much of the server’s resources they can collectively use. The server will now take an
-    # additional commandline parameter ( sys.argv[2] ) that says how many clients may actually be
-    # actively running concurrently (e.g. 5 -- don’t make it too big!). Note that sys.argv[1] will still
-    # be the server’s binding port. You can implement a -v flag if you like, but it’s not required and will
-    # not be graded; your server should not have any output as the default option.
-
-
-# The manager sits in an infinite loop, executing the following pseudocode:
-
-# check the “ running ” threads; if any of them have stopped, remove them from the set.
-# kick = []
-# for t in self.running:
-#     if not t.isAlive(): kick.append(t)
-# for t in kick:
-#     self.running.remove(t)
-
-# check the waiting queue:
-    # if empty, sleep for 1 second and return to the top of the loop;
-    # if it has an item:
-        # check the size of the running set:
-            # if it is full, sleep for 1 second and return to the top of the loop;
-            # if it has space:      
-                # remove the next client thread from the queue
-                # start the thread
-                # add the thread to the running set
-
-# Note: Main program won't terminate while it has a child thread still running.
-# To force all the threads to stop, you will have to use an operating-system kill. 
-# On linux/mac, you can do the following:
-    # ps aux | grep python - gives a list of python processes; find the
-    # server.py process, and note the pid (second column) number (e.g. 12573)
-    # kill <pid> - forces all threads under that process id to stop.
-
 
 
             
